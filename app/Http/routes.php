b1da3cff95841dcc419469b6652a2ddf03fee5f9
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2016-08-11 14:41:37 +0700

    task save

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,6 +27,12 @@ Route::post('/task', function (Request $request) {
             ->withInput()
             ->withErrors($validator);
     }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 /**
