c5cfdda509166a49b474a8862ab8627b08d2e3fa
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2016-08-11 14:37:58 +0700

    add new routes and modify root route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+/**
+ * Show Task Dashboard
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
